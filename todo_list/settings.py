332f2eec8e3bcbde2ee3386797c3595b3a55f178
pratyushmaurya01 <159344283+pratyushmaurya01@users.noreply.github.com>
2025-04-01 00:20:52 +0530

    Update settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -126,7 +126,5 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
